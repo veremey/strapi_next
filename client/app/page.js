3c5c1180dad3aea6db2eaea8bf738ba26d972a95
Sergey Veremiychuk <veremey4uk@gmail.com>
2023-06-29 20:52:02 +0200

    fixed get data from DB | structurred code

--- client/app/page.js
+++ client/app/page.js
@@ -1,11 +1,31 @@
 import PageContainer from "@/components/PageContainer";
-import Hello from "@/components/Hello"
+import Description from "@/components/Description";
+import Title from "@/components/Title";
+import Main from "@/components/Main";
 
 export default async function Home() {
- 
+  const data = await getPosts();
+
   return (
     <PageContainer>
-      <Hello />
+      <Main>
+        <Title>Checkin from others (for me)</Title>
+        <Description>a recipe for disaster</Description>
+
+        {data.data.map((post) => (
+          <div key={post.attributes.slug}>{post.attributes.title}</div>
+        ))}
+      </Main>
     </PageContainer>
   );
 }
+
+  async function getPosts() {
+    try {
+      const response = await fetch(`${process.env.API_URL}/api/posts`);
+      const data = await response.json();
+      return data
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
